52d0b64b92ca9f21e4283d2cb4b716bff652f86c
silvano-paulino <silvanocavimbi20@gmail.com>
2025-08-08 16:26:08 +0100

    chore: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,40 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@nuxt/image"],
   css: ["@/assets/css/main.css"],
   components: true,
+  app: {
+    head: {
+      link: [
+        { rel: "icon", href: "/favicon/favicon.ico" },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon/favicon-16x16.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon/favicon-32x32.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "/favicon/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "192x192",
+          href: "/favicon/android-chrome-192x192.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "512x512",
+          href: "/favicon/android-chrome-512x512.png",
+        },
+        { rel: "manifest", href: "/favicon/site.webmanifest" },
+      ],
+    },
+  },
 });
