a1d070c14e66fa082f62a82e255852fb392157c7
silvano-paulino <silvanocavimbi20@gmail.com>
2025-08-15 17:39:44 +0100

    style: the start of events pages

---
+++ app/pages/events.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { NuxtImg } from '#components';
+import TheParagraph from '~/components/TheParagraph.vue';
+
+useSeoMeta({
+  title: 'Eventos OMVA',
+  description:
+    'Descubra os próximos eventos e atividades que a nossa organização está a planear.',
+});
+</script>
+
+<template>
+  <div class="max-w-8xl mx-auto py-12 md:py-20 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-5xl mx-auto">
+      <TheH1>Próximos Eventos</TheH1>
+    </div>
+    <div class="md:h-[30rem] bg-light">
+      <div class="grid grid-cols-1 lg:grid-cols-3">
+        <div class="lg:col-span-2">
+          <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            <div class="md:h-[30rem]">
+              <NuxtImg
+                src="/images/events/event-workshop-bovinocultura.jpeg"
+                class="h-full w-full object-cover"
+              />
+            </div>
+            <div class="grid items-center p-5">
+              <div>
+                <h3 class="mb-5 font-medium text-2xl text-brown">The Title</h3>
+                <TheParagraph class="max-w-80">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+                  consequuntur cumque, in nisi non reprehenderit placeat
+                  consequatur quam saepe rerum blanditiis vitae delectus fugiat
+                  quis quos ullam totam sit ad.
+                </TheParagraph>
+
+                <hr class="max-w-16 border-[1px] my-7" />
+
+                <TheButton class="bg-white">Saber Mais</TheButton>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="">content right</div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ content/events-content.md
@@ -0,0 +1,11 @@
+WORKSHOP SOBRE BOVINOCULTURA – HUAMBO
+26 e 27 de Setembro de 2025
+
+Uma oportunidade única para médicos veterinários, técnicos e profissionais do sector partilharem experiências, aprofundarem conhecimentos e contribuírem para o desenvolvimento sustentável da pecuária bovina em Angola.
+
+Este encontro será um espaço de formação, debate e troca de ideias, fortalecendo o papel do médico veterinário como agente essencial na produtividade, sanidade e bem-estar animal.
+
+Local: Huambo
+Público-alvo: Médicos Veterinários, Técnicos de Produção Animal, Estudantes e Empresários do Sector Pecuário
+
+Participe e faça a diferença no futuro da bovinocultura angolana!
