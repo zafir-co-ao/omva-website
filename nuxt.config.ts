2a3db8419f1daca86cca58553805ed28d7be7c97
silvano-paulino <silvanocavimbi20@gmail.com>
2026-06-02 14:01:01 +0100

    chore: remove unused runtime configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@nuxt/image"],
-  css: ["@/assets/css/main.css"],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxt/image'],
+  css: ['@/assets/css/main.css'],
   sourcemap: {
     server: true,
     client: true,
@@ -15,42 +15,41 @@ export default defineNuxtConfig({
     public: {
       antboxUrl: process.env.ANTBOX_URL,
       antboxTenant: process.env.ANTBOX_TENANT,
-      filekeyPath: process.env.FILE_KEY_PATH,
     },
   },
   app: {
     head: {
       link: [
-        { rel: "icon", href: "/favicon/favicon.ico" },
+        { rel: 'icon', href: '/favicon/favicon.ico' },
         {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          href: "/favicon/favicon-16x16.png",
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon/favicon-16x16.png',
         },
         {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
-          href: "/favicon/favicon-32x32.png",
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon/favicon-32x32.png',
         },
         {
-          rel: "apple-touch-icon",
-          href: "/favicon/apple-touch-icon.png",
+          rel: 'apple-touch-icon',
+          href: '/favicon/apple-touch-icon.png',
         },
         {
-          rel: "icon",
-          type: "image/png",
-          sizes: "192x192",
-          href: "/favicon/android-chrome-192x192.png",
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '192x192',
+          href: '/favicon/android-chrome-192x192.png',
         },
         {
-          rel: "icon",
-          type: "image/png",
-          sizes: "512x512",
-          href: "/favicon/android-chrome-512x512.png",
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '512x512',
+          href: '/favicon/android-chrome-512x512.png',
         },
-        { rel: "manifest", href: "/favicon/site.webmanifest" },
+        { rel: 'manifest', href: '/favicon/site.webmanifest' },
       ],
     },
   },
